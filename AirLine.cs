9312602db964e76fba21c2475dcb00ec0d584c10
Ilya Budnik <ilyabudnikxs@mail.ru>
2020-09-21 22:15:11 +0300

    Finish

---
+++ AirLine.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    partial class AirLine
+    {
+        private int id = 0;
+        private string Destination = "";
+        private short NumberOfAirline = 0;
+        private int TypeAircraft = 0;
+        private string Time = "";
+        private string DaysOfWeek = "";
+        private static int size = 0;
+        private readonly uint AirId;
+
+        public static void sizeoff()
+        {
+            Console.WriteLine("Количество объектов:" + size);
+        }
+
+        public int ID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string DestinationC
+        {
+            get { return this.Destination; }
+            set { this.Destination = value; }
+        }
+
+        public short NumberOfAirlineC
+        {
+            get { return this.NumberOfAirline; }
+            set { this.NumberOfAirline = value; }
+
+        }
+
+        public int TypeAircraftC
+        {
+            get { return this.TypeAircraft; }
+            set { this.TypeAircraft = value; }
+
+        }
+        public string TimeC
+        {
+            get { return this.Time; }
+            set { this.Time = value; }
+        }
+
+        public string DaysOfWeekC
+        {
+            get { return this.DaysOfWeek; }
+            set { this.DaysOfWeek = value; }
+        }
+        public AirLine()
+        {
+            this.id = 0;
+            this.Destination = "no";
+            this.NumberOfAirline = 0;
+            this.TypeAircraft = 0;
+            this.TimeC = "no";
+            this.DaysOfWeek = "";
+            this.AirId = MakeHash(id, Destination, NumberOfAirline, TypeAircraft);
+            size++;
+        }
+
+        public AirLine(int iden, string m, short mod, int y, string col, string c)
+        {
+            this.id = iden;
+            this.Destination = m;
+            this.NumberOfAirline = mod;
+            this.TypeAircraft = y;
+            this.TimeC = col;
+            this.DaysOfWeek = c;
+            this.AirId = MakeHash(id, Destination, NumberOfAirline, TypeAircraft);
+            size++;
+        }
+
+
+
+        public AirLine(int iden, string m, int y)
+        {
+            this.id = iden;
+            this.Destination = m;
+            this.TypeAircraft = y;
+            this.AirId = MakeHash(id, Destination, NumberOfAirline, TypeAircraft);
+            size++;
+        }
+    }
+    public partial class AirLine
+    {
+        public uint MakeHash(int id, string Destination, short NumberOfAirline, int TypeAircraft)
+        {
+            int intRes = Destination.Length + TypeAircraft * 9 - id;
+            uint res = (uint)intRes;//явное преобразование
+            return res;
+        }
+
+    }
+    public class NewClass : System.Object//унаследование
+    {
+        public override bool Equals(object obj)//переопределение метода
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return base.Equals(obj);//ссылка  на родителя
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab_3
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-           }
+            object[] ListOfAirplanes = new object[6]
+            {
+                new AirLine(1, "Minsk", 1, 32, "12:30", "Wensday"),
+                   new AirLine(2, "Berlin", 2, 34, "12:23", "Sunday"),
+                      new AirLine(23, "Moscow", 3, 553, "2:30", "Tuesday"),
+                         new AirLine(123, "Washingtone", 4, 644, "8:30", "Wensday"),
+                            new AirLine(44, "Minsk", 1, 900, "6:30", "Friday"),
+                               new AirLine(132, "Minsk", 3123, 777, "7:30", "Friday")
+            };
+            AirLine.sizeoff();
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("Список самолётов до Минска:");
+            Console.WriteLine("__________________________________");
+            foreach (AirLine Destination in ListOfAirplanes)
+            {
+                if(Destination.DestinationC == "Minsk")
+                {
+                   
+                    Console.WriteLine(Destination.ID +"\t"+ Destination.DestinationC +"\t" + Destination.TimeC);
+                    
+                }
+            }
+            Console.WriteLine("__________________________________");
+
+            Console.WriteLine("\n__________________________________");
+            Console.WriteLine("Список самолётов для Понедельника:");
+            Console.WriteLine("__________________________________");
+            foreach (AirLine DaysOfWeek in ListOfAirplanes)
+            {
+                if (DaysOfWeek.DaysOfWeekC == "Wensday")
+                {
+
+                    Console.WriteLine(DaysOfWeek.ID + "\t" + DaysOfWeek.DestinationC + "\t" + DaysOfWeek.TimeC);
+
+                }
+            }
+            Console.WriteLine("__________________________________");
+            Console.WriteLine(Equals(ListOfAirplanes[0], ListOfAirplanes[1]));
+            Console.WriteLine(ListOfAirplanes.Equals(ListOfAirplanes));
+            Console.ReadLine();
+        }
     }
+    
 }
